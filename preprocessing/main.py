10eb356acc9d58d3c410f942bce329f4751355ea
Samuel Friedman <samuelmf1@gmail.com>
2024-08-06 17:56:25 -0400

    Create main.py

---
+++ preprocessing/main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+import glob
+from openai import OpenAI
+from preprocessing import Preprocess
+
+def main():
+
+    directory_of_sumstats = '' # YOUR INPUT PATH
+    output_directory = '' # YOUR OUTPUT PATH
+
+    ft = Preprocess(
+        client = OpenAI(
+            organization = os.getenv('OPENAI_ORGANIZATION'), # see README.md
+            project = os.getenv('OPENAI_PROJECT'),
+            api_key = os.getenv('OPENAI_API_KEY')
+        ), 
+        out_dir=output_directory # sets output path
+    )
+
+    # optional parameters
+    ft.significance_threshold = 5e-8 # default value is already 5e-8
+    ft.ancestry = 'EUR' # default value is 'EUR' but can be statically
+
+    for path in glob.glob(directory_of_sumstats + '/*'):
+
+        # loop through all files in input path
+        print(path)
+
+        # run loadmap_sumstats_table
+        res = ft.loadmap_sumstats_table(
+            path,
+            verbose=False # set to True if you want it to print what it's doing
+        )
+        # returns 1 upon success
+
+        # create leadsnp table if GPT was able to detect, map, and rearrange columns
+        if res == 1:
+            ft.create_leadsnp_table(verbose=False)
+
+
+if __name__ == '__main__':
+    main()
